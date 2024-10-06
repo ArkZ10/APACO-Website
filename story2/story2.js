c2bf763a0d7f02e08609a3ec2432697c4744fc8a
Yeftha Joshua Ezekiel <jefthajoshua9@gmail.com>
2024-10-06 11:29:37 +0700

    Update story2.js

--- story2/story2.js
+++ story2/story2.js
@@ -18,7 +18,7 @@ document.querySelector('.button3').addEventListener('click', function(e) {
     document.querySelector('#sec5').scrollIntoView({ behavior: 'smooth' });
 });
 
-document.getElementById('liver').addEventListener('click', function(e) {
+document.getElementById('vision').addEventListener('click', function(e) {
     e.preventDefault(); 
     //window.location.href = '../RR8/qna/q1.html';
     window.location.href = '../RR23/qna/q1.html';
